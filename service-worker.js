9561ba995251da25266aaf7d55ee6890d9d6bec5
Ivan Panchenko <ivanpanchenkorulit@gmail.com>
2021-03-15 18:03:09 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.2864373d6bc61c7e48f9e43b143db318.js"
+  "./precache-manifest.d153df0a9688df68e746ec2c9d66b582.js"
 );
 
 self.addEventListener('message', (event) => {
